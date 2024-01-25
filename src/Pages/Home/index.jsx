6e05632018f2c3f4a305056fbc0c8940c08766c2
Jose Stiven Correa Valoyes <103680054+JoseCorreaV@users.noreply.github.com>
2024-01-25 14:49:22 -0500

    Update index.jsx

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -6,22 +6,39 @@ import { ShoppingCartContext } from '../../Context'
 function Home() {
   const context = useContext(ShoppingCartContext)
 
-  const renderView = () => {
-    if (context.filteredItems?.length > 0) {
-      return (
-        context.filteredItems?.map(item => (
-          <Card key={item.id} data={item} />
-        ))
-      )
-    } else {
-      return (
-        <div>We don't have anything :(</div>
-      )
-    }
+const renderView = () => {
+  const filteredItems = context.filteredItems;
+
+  if (filteredItems?.length > 0) {
+    return (
+      filteredItems?.map(item => (
+        <Card key={item.id} data={item} />
+      ))
+    );
+  } else {
+    return (
+      <div>We don't have anything :(</div>
+    );
   }
+}
+
+  const renderView = () => {
+  return (
+    context.filteredItems?.length > 0
+      ? renderCards(context.filteredItems)
+      : <div>We don't have anything :(</div>
+  );
+}
+
+const renderCards = (items) => {
+  return items.map(item => (
+    <Card key={item.id} data={item} />
+  ));
+}
+
 
   return (
-    <div> <center>
+    <div style={{ textAlign: 'center' }}> <center>
       <div className='flex items-center justify-center relative w-80 mb-4'>
         <h1 className='font-medium text-xl'>Exclusive Products</h1>
       </div>
